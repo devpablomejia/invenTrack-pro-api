d1bcb5e51fb942d8a672657a2991e32f9a04e230
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-26 17:41:29 -0500

    feat: Reorganizar estructura de carpetas y agregar manejo de rutas y errores 404

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { config } from "./config/config.js";
+import { config } from "./infra/config/config.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const API_VERSION = config.api_version;
 const PORT = config.port;
-
 const app = express();
 
 app.use(express.json());
+app.use(API_VERSION, authRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Endpoint not found" });
+});
 app.listen(PORT, () => { console.log(`SERVER RUNING ON PORT http://localhost:${PORT}${API_VERSION}`) });
