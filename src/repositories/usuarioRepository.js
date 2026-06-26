df54f4dcd818ffad2bcc1a6262f7d9c3eb8157e2
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-26 17:36:42 -0500

    feat: Agregar funciones para gestionar usuarios en la base de datos

---
+++ src/repositories/usuarioRepository.js
@@ -0,0 +1,35 @@
+import { pool } from "../infra/config/db.js";
+
+export const saveUsuario = async (nombre, email, password, rolId) => {
+    const [result] = await pool.query(
+        "INSERT INTO usuarios (nombre, email, password, role_id) VALUES (?, ?, ?, ?)",
+        [nombre, email, password, rolId]
+    );
+    return result;
+};
+
+export const updateUsuario = async (nombre, email, password, rolId, id) => {
+    const [result] = await pool.query(
+        "UPDATE usuarios SET nombre = IFNULL (?, nombre), email = IFNULL (?, email), password = IFNULL (?, password), role_id = IFNULL (?, role_id) WHERE id = ? )",
+        [nombre, email, password, rolId, id]
+    );
+    return result;
+};
+
+export const findUsuarioById = async (id) => {
+    const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
+    return rows;
+};
+
+export const findUsuarioByEmail = async (email) => {
+    const [rows] = await pool.query("SELECT * FROM usuarios WHERE email = ?", [email]);
+    return rows;
+};
+
+export const existUsuarioByEmail = async (email) => {
+    const [rows] = await pool.query("SELECT * FROM usuarios WHERE email = ?", [email]);
+    if (rows > 0) {
+        return true;
+    }
+    return false;
+};
