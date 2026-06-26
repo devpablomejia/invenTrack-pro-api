b89f30e4de55c09de50597c197575a31b1e8bfaa
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-26 16:02:47 -0500

    feat: Agregar script de inserción de datos para tablas de categorías y roles

---
+++ db/seedTables.sql
@@ -0,0 +1,11 @@
+--- datos para la tabla categorias
+INSERT INTO categorias (nombre, descripcion) VALUES 
+('Procesadores', 'Unidades Centrales de Procesamiento (CPU) para servidores y escritorios.'),
+('Tarjetas Gráficas', 'Componentes de procesamiento visual y aceleración de IA.'),
+('Memoria RAM', 'Módulos de memoria volátil DDR4 y DDR5.'),
+('Almacenamiento', 'Discos duros mecánicos (HDD) y unidades de estado sólido (SSD) NVMe.'),
+('Placas Base', 'Tarjetas madre para arquitecturas Intel y AMD.'),
+('Fuentes de Poder', 'Unidades de alimentación energética con certificación 80 Plus.');
+
+--- datos para la tabla roles
+INSERT INTO roles (nombre) VALUES  ('ADMIN'), ('MANAGER'), ('VIEWER');
