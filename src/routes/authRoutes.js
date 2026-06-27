77095fdbcaa63ea81e5c704c89c85dac857d18bb
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 14:52:40 -0500

    feat: agregar middleware de validación para rutas de autenticación

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import { createValidationMiddleware } from "../middlewares/createValidationMiddleware.js"
+import { createUsuarioShema, authenticateUsuarioShema } from "../utils/schemas/usuariosShemas.js";
 import { registrarUsuario, autenticarUsuario } from "../controllers/authController.js";
 
 const router = Router();
 
-router.post("/auth/registro", registrarUsuario);
-router.post("/auth/autenticar", autenticarUsuario);
+router.post("/auth/registro",
+    createValidationMiddleware({ body: createUsuarioShema }),
+    registrarUsuario
+);
+router.post("/auth/autenticar",
+    createValidationMiddleware({ body: authenticateUsuarioShema }),
+    autenticarUsuario);
 
 export default router;
