1be476200d7d30a26d84d26d461986e2b803b8ed
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 11:13:33 -0500

    feat: agregar función isMatch para comparar contraseñas encriptadas

--- src/infra/security/bycript.js
+++ src/infra/security/bycript.js
@@ -4,3 +4,7 @@ export const encodePassword = async (password) => {
     const saltRounds = 10;
     return await bcrypt.hash(password, saltRounds);
 };
+
+export const isMatch = async (password, hashPassword) => {
+    return await bcrypt.compare(password, hashPassword);
+};
