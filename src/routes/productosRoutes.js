7e99460a3a79c4367e6ccfb82a54c174a91ef7df
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 15:18:02 -0500

    feat: agregar middleware de validación para las rutas de productos

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { getPageProductos, createProducto } from "../controllers/productosController.js";
 import { verifyToken, authorizeRoles } from "../middlewares/auth.js";
+import { createValidationMiddleware } from "../middlewares/createValidationMiddleware.js";
+import { createProductoShema } from "../utils/schemas/productosSchemas.js";
+import { paginationQuerySchema } from "../utils/schemas/paginationShemas.js";
 
 const router = Router();
 
-router.get("/productos", verifyToken, authorizeRoles('ADMIN', 'MANAGER', 'VIEWER'), getPageProductos);
-router.post("/productos", verifyToken, authorizeRoles('ADMIN', 'MANAGER'), createProducto);
+router.get("/productos",
+    verifyToken,
+    authorizeRoles('ADMIN', 'MANAGER', 'VIEWER'),
+    createValidationMiddleware({ query: paginationQuerySchema }),
+    getPageProductos
+);
+router.post("/productos",
+    verifyToken,
+    authorizeRoles('ADMIN', 'MANAGER'),
+    createValidationMiddleware({ body: createProductoShema }),
+    createProducto);
 
 export default router;
