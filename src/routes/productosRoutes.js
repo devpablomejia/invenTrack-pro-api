36ee99850bf0ec9d36912003eaf1beabf73d25a1
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 13:59:16 -0500

    feat: agregar ruta para crear nuevos productos

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getPageProductos } from "../controllers/productosController.js";
+import { getPageProductos, createProducto } from "../controllers/productosController.js";
 import { verifyToken, authorizeRoles } from "../middlewares/auth.js";
 
 const router = Router();
 
 router.get("/productos", verifyToken, authorizeRoles('ADMIN', 'MANAGER', 'VIEWER'), getPageProductos);
+router.post("/productos", verifyToken, authorizeRoles('ADMIN', 'MANAGER'), createProducto);
 
 export default router;
