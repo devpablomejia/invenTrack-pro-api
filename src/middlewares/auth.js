da187914bd6cc2f5ae28c63f9dff1676275c9e7b
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 12:15:11 -0500

    feat: agregar middleware de autenticación y autorización de roles

---
+++ src/middlewares/auth.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({ message: "Access denied. No token provided." });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid or expired token." });
+    }
+};
+
+export const authorizeRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({ message: "No autorizado. Identidad desconocida." });
+        }
+        if (!allowedRoles.includes(req.user.role)) {
+            console.log(req.user.role)
+            return res.status(403).json({
+                message: `Acceso denegado. Se requiere uno de los siguientes roles: [${allowedRoles.join(', ')}]`
+            });
+        }
+        next();
+    };
+};
