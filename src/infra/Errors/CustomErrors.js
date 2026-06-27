283d76f19235ab21f2ddb5469465ec896e0d8c27
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 10:54:29 -0500

    feat: agregar nuevo error para validacion de existencia de usuarios y cambiar codigo http para ResourceNotFound

--- src/infra/Errors/CustomErrors.js
+++ src/infra/Errors/CustomErrors.js
@@ -1,6 +1,13 @@
 export class ResourceNotFoundError extends Error {
     constructor(message) {
         super(message);
+        this.statusCode = 404;
+    }
+};
+
+export class EmailAlreadyRegisteredError extends Error {
+    constructor(message) {
+        super(message);
         this.statusCode = 400;
     }
-}
+};
