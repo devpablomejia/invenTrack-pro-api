e9d05e6837774426ec5ffbfe869057245005ed65
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 11:56:38 -0500

    feat: agregar función authenticateUsuario para autenticar usuarios y generar tokens JWT

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -1,10 +1,12 @@
 import * as bycript from '../infra/security/bycript.js';
 import * as usuarioRepository from '../repositories/usuarioRepository.js';
 import { findRolByNombre } from '../repositories/rolRepository.js';
-import { EmailAlreadyRegisteredError, ResourceNotFoundError } from '../infra/Errors/CustomErrors.js';
+import { EmailAlreadyRegisteredError, ResourceNotFoundError, UnauthorizeError } from '../infra/Errors/CustomErrors.js';
+import * as jwtService from '../infra/security/jwt.js';
 
 export const createUsuario = async (nombre, email, password, rol) => {
-    if (usuarioRepository.existUsuarioByEmail(email)) {
+    const isExistUsuario = await usuarioRepository.existUsuarioByEmail(email);
+    if (isExistUsuario) {
         throw new EmailAlreadyRegisteredError('El email ya esta registrado');
     };
     const rolId = await extractRol(rol);
@@ -12,9 +14,21 @@ export const createUsuario = async (nombre, email, password, rol) => {
     return await usuarioRepository.saveUsuario(nombre, email, hashedPassword, rolId);
 };
 
+export const authenticateUsuario = async (email, password) => {
+    const usuario = await usuarioRepository.loadUsuarioByEmail(email);
+    const isMatch = await bycript.isMatch(password, usuario[0].password);
+    if (usuario === 0 || !usuario || !usuario[0].id) {
+        throw new UnauthorizeError('El email no esta registrado');
+    };
+    if (!isMatch) {
+        throw new UnauthorizeError('La contraseña es incorrecta');
+    };
+    const tokenGenerated = jwtService.generateToken(usuario[0]);
+    return tokenGenerated;
+}
+
 const extractRol = async (rol) => {
     const roleResult = await findRolByNombre(rol);
-
     if (!roleResult || roleResult.length === 0 || !roleResult[0].id) {
         throw new ResourceNotFoundError('El rol especificado no existe en el sistema');
     }
