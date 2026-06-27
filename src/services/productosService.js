bb0390cfd79e7c6283f158ad5d597045371ed3a7
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 13:58:58 -0500

    feat: agregar función createProducto para crear nuevos productos con validación de categoría

--- src/services/productosService.js
+++ src/services/productosService.js
@@ -1,5 +1,15 @@
 import * as productosRepository from "../repositories/productosRepository.js";
+import { findCategoriaByNombre } from "../repositories/categoriaRepository.js";
 
 export const getPageProductos = async (page, limit) => {
     return await productosRepository.findPageProductos(page, limit);
 };
+
+export const createProducto = async (sku, nombre, descripcion, precio, stockActual, categoria) => {
+    const catgResult = await findCategoriaByNombre(categoria);
+    if (!catgResult || catgResult.length === 0 || !catgResult[0].id) {
+        throw new ResourceNotFoundError('La categoria especificada no existe en el sistema');
+    }
+    const categoriaId = catgResult[0].id;
+    return await productosRepository.saveProducto(sku, nombre, descripcion, precio, stockActual, categoriaId);
+}; 
