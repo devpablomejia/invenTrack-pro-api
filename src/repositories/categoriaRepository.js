29191bf0bf0050fe6bb2a2006403c7426900c17d
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-27 13:58:29 -0500

    feat: hacer que la función findCategoriaByNombre sea asíncrona

--- src/repositories/categoriaRepository.js
+++ src/repositories/categoriaRepository.js
@@ -1,6 +1,6 @@
 import { pool } from "../infra/config/db.js";
 
-export const findCategoriaByNombre = (nombre) => {
+export const findCategoriaByNombre = async (nombre) => {
     const [rows] = await pool.query("SELECT * FROM categorias WHERE nombre = ?", [nombre]);
     return rows;
 };
