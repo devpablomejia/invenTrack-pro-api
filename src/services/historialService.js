4948bbc347832f2aa4e6e38a3cfce19604711a49
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-28 13:46:50 -0500

    feat: agregar función registrarHistorialMovimiento para registrar movimientos de stock

---
+++ src/services/historialService.js
@@ -0,0 +1,29 @@
+import * as historialRepository from "../repositories/historialRepository.js";
+import { findTipoByNombre } from "../repositories/tipoMovimientoRepository.js";
+import { findUsuarioById } from "../repositories/usuarioRepository.js";
+import { findProductoByNombre } from "../repositories/productosRepository.js";
+import { ResourceNotFoundError, UsuarioNotFoundError } from "../infra/errors/CustomErrors.js";
+
+export const registrarHistorialMovimiento = async (producto, usuarioId, tipo, cantidad, motivo) => {
+    const productoFormatt = producto.trim();
+    const tipoFormatt = tipo.trim().toUpperCase();
+    const productoResult = await findProductoByNombre(productoFormatt);
+    const tipoResult = await findTipoByNombre(tipoFormatt);
+    const usuarioResult = await findUsuarioById(usuarioId);
+
+    if (!tipoResult || tipoResult.length === 0 || !tipoResult[0].id) {
+        throw new ResourceNotFoundError('El tipo especificado no existe en el sistema');
+    }
+    if (!usuarioResult || usuarioResult.length === 0 || !usuarioResult[0].id) {
+        throw new ResourceNotFoundError('El usuario no existe');
+    }
+    if (!productoResult || productoResult.length === 0 || !productoResult[0].id) {
+        throw new ResourceNotFoundError('El producto especificado no existe en el sistema');
+    }
+
+    const tipoId = tipoResult[0].id;
+    const usuarioIdDb = usuarioResult[0].id;
+    const productoId = productoResult[0].id;
+
+    return await historialRepository.saveMovimientoStock(productoId, usuarioIdDb, tipoId, cantidad, motivo);
+};
