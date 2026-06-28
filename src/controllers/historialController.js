f5ae20e39eca64f7d549dd67ca24ad0619da2312
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-28 14:04:17 -0500

    feat: agregar función getPageMovimientos para paginar movimientos de stock

--- src/controllers/historialController.js
+++ src/controllers/historialController.js
@@ -1,6 +1,26 @@
 import * as historialService from "../services/historialService.js";
 import { errorReturn } from "../infra/errors/errorReturn.js";
 
+export const getPageMovimientos = async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const limit = req.query.limit || 10;
+        const { movimientos, totalItems, totalPages } = await historialService.getPageMovimientos(page, limit);
+        return res.status(200).json({
+            status: "success",
+            data: movimientos,
+            pagination: {
+                page: parseInt(page),
+                limit: parseInt(limit),
+                totalItems: totalItems,
+                totalPages: totalPages
+            }
+        });
+    } catch (error) {
+        errorReturn(error, res);
+    }
+};
+
 export const registrarHistorialMovimiento = async (req, res) => {
     try {
         const { id } = req.user;
