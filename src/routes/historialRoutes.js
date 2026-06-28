d86454062002183258a69f913d9c0d5725e3300c
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-28 14:04:23 -0500

    feat: agregar ruta para obtener movimientos de stock paginados

--- src/routes/historialRoutes.js
+++ src/routes/historialRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registrarHistorialMovimiento } from "../controllers/historialController.js";
+import { registrarHistorialMovimiento, getPageMovimientos } from "../controllers/historialController.js";
 import { verifyToken, authorizeRoles } from "../middlewares/auth.js";
 import { createValidationMiddleware } from "../middlewares/createValidationMiddleware.js";
 import { historialSchema } from "../utils/schemas/historialShemas.js";
@@ -7,6 +7,12 @@ import { paginationQuerySchema } from "../utils/schemas/paginationShemas.js";
 
 const router = Router();
 
+router.get("/movimientos",
+    verifyToken,
+    authorizeRoles('ADMIN', 'MANAGER', 'VIEWER'),
+    createValidationMiddleware({ query: paginationQuerySchema }),
+    getPageMovimientos
+);
 router.post("/movimientos",
     verifyToken,
     authorizeRoles('ADMIN', 'MANAGER'),
