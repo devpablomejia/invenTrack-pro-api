17da7d64a1ecd36c6ccbdc95fecd41f9b6133e40
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-28 13:47:20 -0500

    feat: agregar rutas para el historial de movimientos de stock

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "./infra/config/config.js";
 import authRoutes from "./routes/authRoutes.js";
 import productosRoutes from "./routes/productosRoutes.js";
+import historialRoutes from "./routes/historialRoutes.js";
 
 const API_VERSION = config.api_version;
 const PORT = config.port;
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(API_VERSION, authRoutes);
 app.use(API_VERSION, productosRoutes);
+app.use(API_VERSION, historialRoutes);
 
 app.use((req, res, next) => {
     res.status(404).json({ message: "Endpoint not found" });
