dd74fbb438e23500135ba74cb2ed08c5fa804601
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-28 11:46:51 -0500

    feat: agregar función para obtener un producto por ID con esquema de validación

--- src/routes/productosRoutes.js
+++ src/routes/productosRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getPageProductos, createProducto } from "../controllers/productosController.js";
+import { getPageProductos, createProducto, getProductoById} from "../controllers/productosController.js";
 import { verifyToken, authorizeRoles } from "../middlewares/auth.js";
 import { createValidationMiddleware } from "../middlewares/createValidationMiddleware.js";
-import { createProductoShema } from "../utils/schemas/productosSchemas.js";
+import { createProductoSchema, productoIdShema } from "../utils/schemas/productosSchemas.js";
 import { paginationQuerySchema } from "../utils/schemas/paginationShemas.js";
 
 const router = Router();
@@ -13,10 +13,16 @@ router.get("/productos",
     createValidationMiddleware({ query: paginationQuerySchema }),
     getPageProductos
 );
+router.get("/productos/:productoId",
+    verifyToken,
+    authorizeRoles('ADMIN', 'MANAGER', 'VIEWER'),
+    createValidationMiddleware({ params: productoIdShema }),
+    getProductoById
+);
 router.post("/productos",
     verifyToken,
     authorizeRoles('ADMIN', 'MANAGER'),
-    createValidationMiddleware({ body: createProductoShema }),
+    createValidationMiddleware({ body: createProductoSchema }),
     createProducto);
 
 export default router;
