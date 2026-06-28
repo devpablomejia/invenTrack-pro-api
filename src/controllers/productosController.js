013ed548181fabf5d48442c6beacab05c1a47846
pablo M. Mejia <165319132+devpablomejia@users.noreply.github.com>
2026-06-28 11:44:42 -0500

    feat: agregar función para obtener un producto por ID

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -21,6 +21,19 @@ export const getPageProductos = async (req, res) => {
     }
 };
 
+export const getProductoById = async (req, res) => {
+    try {
+        const { productoId } = req.params;
+        const producto = await productosService.getProductoById(productoId);
+        if (producto.length === 0 || !producto) {
+            res.status(404).json({ message: `El Producto con el id: ${productoId} no existe` });
+        };
+        res.status(200).json(producto[0]);
+    } catch (error) {
+        errorReturn(error, res);
+    }
+};
+
 export const createProducto = async (req, res) => {
     try {
         const { sku, nombre, descripcion, precio, stockActual, categoria } = req.body;
